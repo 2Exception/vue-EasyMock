da52da79b9c78d81274d8aa597f6f0a4d98be46a
Damon <web2016vip@gmail.com>
2018-04-19 11:52:02 +0800

    add 404 component

---
+++ src/components/page404.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h2>Error</h2>
+    <p>Looks like something broke!</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "page404"
+  }
+</script>
+
+<style scoped lang="less">
+  .container {
+    width: 1020px;
+    margin: 40px auto;
+    h2 {
+      font: 700 30px/50px '';
+    }
+    p {
+      font-size: 16px;
+      color: #666;
+    }
+  }
+
+</style>
